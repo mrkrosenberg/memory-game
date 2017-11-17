aad94e93b3196eb694ac71de22effb86ae5268e4
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-11-17 15:47:55 -0700

    added functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+cardsInPlay.push(cards[cardId]);
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardOne);
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardTwo);
-
+var flipCard = function(cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("That's a match!");
-	} 	
-else { 
+	} else { 
 	alert("Sorry, try again!")
 	}
+}	
 }
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
