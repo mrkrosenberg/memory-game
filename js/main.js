ed2f56913c7904c84862a8de338498bf9703add9
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-11-17 19:56:46 -0700

    added objects

--- js/main.js
+++ js/main.js
@@ -1,30 +1,59 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds", 
+	cardImage: "images/king-of-diamonds"
+}
+];
 var cardsInPlay = [];
 
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+//console.log("You found a match!");
 } else {
-console.log("Sorry, try again.");
+//console.log("Sorry, try again.");
+	//wasn't sure which assignment the console.log statements came from 
 }
 }
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("That's a match!");
 	} else { 
 	alert("Sorry, try again!")
 	}
-}	
+}
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);	
 }
 
-flipCard(3);
-flipCard(2);
+flipCard(0);
+flipCard(1);
+
+
+
+
+
 
 
 
